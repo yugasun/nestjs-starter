178c451c136047c84ee9fdab4eb4aa7c934e25a4
yugasun <yuga_sun@163.com>
2019-09-02 14:05:04 +0800

    feat: add test database init

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ const dbConfig: TypeOrmModuleOptions = {
   host: '127.0.0.1',
   port: 3306,
   username: 'root',
-  password: 'test',
+  password: 'root',
   database: 'test',
   entities: [modelPath],
   synchronize: true,
